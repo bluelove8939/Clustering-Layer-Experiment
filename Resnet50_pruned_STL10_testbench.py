fcd9e0e4b84d90221479690601411eafb260e38c
Seongwook Kim <bluelove8939@naver.com>
2022-07-13 18:23:28 +0900

    Update testbench

--- Resnet50_pruned_STL10_testbench.py
+++ Resnet50_pruned_STL10_testbench.py
@@ -9,4 +9,4 @@ loss_fn = normal.loss_fn
 optimizer = normal.optimizer
 
 pmodule = PruneModule(tuning_dataloader=tuning_dataloader, loss_fn=loss_fn, optimizer=optimizer)
-pmodule.prune_model(model, target_amount=0.3, threshold=1, step=0.1, max_iter=5, pass_normal=False)
+pmodule.prune_model(model, target_amount=0.3, threshold=1, step=0.1, max_iter=5, pass_normal=False, verbose=1)
